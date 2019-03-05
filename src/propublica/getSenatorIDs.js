7ac291fb6fce3b459bf47f308f9422310824a089
Wade <wade@astuteape.com>
2019-03-04 19:15:08 -0800

    Add axios call for senator IDs

---
+++ src/propublica/getSenatorIDs.js
@@ -0,0 +1,25 @@
+// For development use only. Replaced with environment variables from the
+// context object in production
+require("dotenv").config();
+const axios = require("axios");
+
+/** This function retrieves current senators from ProPublica by State and
+ *  returns their ProPublica Member IDs. This can be passed back to ProPublica
+ *  for details about senators such as their contact details and voting record.
+ */
+const getSenatorIDs = state => {
+  return axios({
+    method: `get`,
+    url: `https://api.propublica.org/congress/v1/members/senate/${state}/current.json`,
+    headers: {
+      "X-API-KEY": process.env.PROPUBLICA_API_KEY
+    },
+    withCredentials: false
+  }).then(response => {
+    const senatorOne = response.data.results[0].id;
+    const senatorTwo = response.data.results[1].id;
+    return [senatorOne, senatorTwo];
+  });
+};
+
+module.exports = getSenatorIDs;
